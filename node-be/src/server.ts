622feab907a403ff7038db3d955b39844fa55349
Maddev4 <maddev049@gmail.com>
2025-05-06 18:41:08 -0400

    cors

--- node-be/src/server.ts
+++ node-be/src/server.ts
@@ -16,12 +16,13 @@ const httpServer = createServer(app);
 const port = process.env.PORT || 8087;
 
 // Middleware
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: process.env.CLIENT_URL || "http://localhost:5173",
+//     credentials: true,
+//   })
+// );
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
